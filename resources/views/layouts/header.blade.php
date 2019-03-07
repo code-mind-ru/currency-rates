96391df8a80e47a96d9f6ccff22a7a2a68bd9910
Andrey Titov <a.titov@1september.ru>
2019-03-07 04:42:21 +0300

    update title

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
-<title>Dashboard Template · Bootstrap</title>
-<!-- Bootstrap core CSS -->
+<title>currency-rates</title>
 <link href="/css/bootstrap.min.css" rel="stylesheet" />
-
-
 <style>
     .bd-placeholder-img {
         font-size: 1.125rem;
@@ -22,7 +19,6 @@
         }
     }
 </style>
-<!-- Custom styles for this template -->
 <link href="/css/dashboard.css" rel="stylesheet">
 <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
 
